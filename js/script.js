6f67e10f79f67a5738298b08ac1d176701585404
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-19 14:24:07 -0500

    init

--- js/script.js
+++ js/script.js
@@ -15,9 +15,19 @@ if (navigator.serviceWorker) {
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function start() {
+  const params = new URLSearchParams(document.location.search)
+  const radius = params.get('r')
+
+  document.getElementById("radius").innerHTML =
+    "The radius is: " + radius + " cm."
+
+  const volume = (4.0 * Math.PI * radius ** 3) / 3.0
+  document.getElementById("volume-of-sphere").innerHTML =
+    "The volume of the sphere is: " + volume.toFixed(2) + " cm³."
+
+
 }
+
+
+
